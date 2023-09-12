9b182f72ce16526d890634b60446d3a18c22764a
Soc Virnyl Estela <socvirnyl.estela@gmail.com>
2023-09-12 23:05:42 +0800

    boilerplate: initial boilerplate for reading _service for cargo audit
    Signed-off-by: Soc Virnyl Estela <socvirnyl.estela@gmail.com>

--- cargo/src/audit/mod.rs
+++ cargo/src/audit/mod.rs
@@ -5,3 +5,124 @@
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+use quick_xml as xml;
+use std::io;
+use std::path::Path;
+use std::path::PathBuf;
+
+#[allow(unused_imports)]
+use tracing::{debug, error, info, warn, Level};
+
+#[allow(unused_imports)]
+use crate::cli::{Opts, SrcDir, SrcTar};
+
+#[allow(dead_code)]
+const EXCLUDE_ITEMS: &[&str] = &[
+    // These two are excluded because they are fundamentally
+    // silly and can never be fixed.
+    // https://rustsec.org/advisories/RUSTSEC-2020-0071.html
+    // https://rustsec.org/advisories/RUSTSEC-2020-0159.html
+    "RUSTSEC-2020-0071",
+    "RUSTSEC-2020-0159",
+];
+
+pub enum Src {
+    Tar(SrcTar),
+    Dir(SrcDir),
+}
+
+pub fn read_service(p: &Path) -> Result<Src, io::Error> {
+    // use xml::events::BytesStart;
+    use xml::events::Event;
+    use xml::name::QName;
+    use xml::reader::Reader;
+    if let Ok(mut reader) = Reader::from_file(p) {
+        reader.trim_text(true);
+        // let mut skip_buf = Vec::new();
+        let mut operations = Vec::new();
+        let mut is_target_tag = false;
+        let mut is_srctar = false;
+        let mut is_srcdir = false;
+
+        loop {
+            if let Ok(read_event) = reader.read_event_into(&mut operations) {
+                match read_event {
+                    Event::Start(element) => {
+                        if element.name() == QName(b"service") {
+                            info!(?element, "Found target tag");
+                            for attr in element.attributes() {
+                                if let Ok(attr) = attr {
+                                    println!(
+                                        "Attribute: {} = {}",
+                                        String::from_utf8_lossy(attr.key.local_name().as_ref()),
+                                        String::from_utf8_lossy(&attr.value)
+                                    );
+                                    if attr.key == QName(b"name")
+                                        && attr.value == "cargo_vendor".as_bytes()
+                                    {
+                                        is_target_tag = true;
+                                    }
+                                } else {
+                                    error!(?attr);
+                                    return Err(io::Error::new(
+                                        io::ErrorKind::InvalidData,
+                                        "Attribute is unreadable",
+                                    ));
+                                };
+                            }
+                        } else if element.name() == QName(b"param") && is_target_tag {
+                            for attr in element.attributes() {
+                                if let Ok(attr) = attr {
+                                    println!(
+                                        "Attribute: {} = {}",
+                                        String::from_utf8_lossy(attr.key.local_name().as_ref()),
+                                        String::from_utf8_lossy(&attr.value)
+                                    );
+                                    if attr.key == QName(b"name")
+                                        && attr.value == "srctar".as_bytes()
+                                    {
+                                        is_srctar = true;
+                                    } else if attr.key == QName(b"name")
+                                        && attr.value == "srcdir".as_bytes()
+                                    {
+                                        is_srcdir = true;
+                                    }
+                                } else {
+                                    error!(?attr);
+                                    return Err(io::Error::new(
+                                        io::ErrorKind::InvalidData,
+                                        "Attribute is unreadable",
+                                    ));
+                                };
+                            }
+                        }
+                    }
+                    Event::Text(val) => {
+                        if is_srctar {
+                            let srctar =
+                                PathBuf::from(String::from_utf8_lossy(val.as_ref()).as_ref());
+                            return Ok(Src::Tar(SrcTar { srctar }));
+                        } else if is_srcdir {
+                            let srcdir =
+                                PathBuf::from(String::from_utf8_lossy(val.as_ref()).as_ref());
+                            return Ok(Src::Dir(SrcDir { srcdir }));
+                        }
+                    }
+                    Event::Eof => {
+                        return Err(io::Error::new(
+                            io::ErrorKind::UnexpectedEof,
+                            "Reached end of file?",
+                        ))
+                    }
+                    _ => continue,
+                }
+            }
+        }
+    } else {
+        Err(io::Error::new(
+            io::ErrorKind::InvalidData,
+            "Cannot be converted to reader",
+        ))
+    }
+}
--- cargo/src/bin/cargo_audit.rs
+++ cargo/src/bin/cargo_audit.rs
@@ -24,6 +24,75 @@
 #![deny(clippy::manual_let_else)]
 #![allow(clippy::unreachable)]
 
-fn main() {
-    println!("Todo-uwu");
+use obs_service_cargo::audit;
+use obs_service_cargo::audit::Src;
+use std::error::Error;
+use std::fmt;
+use std::fmt::Debug;
+use std::fmt::Display;
+
+struct AuditError {
+    msg: String,
+}
+
+impl Debug for AuditError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.msg)
+    }
+}
+
+impl Display for AuditError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.msg)
+    }
+}
+
+impl Error for AuditError {}
+
+fn main() -> Result<(), AuditError> {
+    if let Some(arg) = std::env::args().nth(1) {
+        let pathtofile = std::path::PathBuf::from(arg);
+        if let Ok(this) = audit::read_service(pathtofile.as_path()) {
+            // TODO: Use the values to find Cargo.lock
+            // IMPORTANT: There are two ways to do this
+            // 1. We either just get Cargo.lock from vendored tarball; or
+            // 2. We will get Cargo.lock based on srctar and srcdir.
+
+            // NOTE: As for method 1 for Cargo.lock, it's fairly easy to find it.
+            // 1. Extract vendored tarball
+            // 2. Find Cargo.lock
+
+            // NOTE: As for method 2, this one is hard because we may have to copy
+            // logic from cargo_vendor. But instead of finding `Cargo.toml`, we will look for
+            // Cargo.lock
+
+            // NOTE: But what about `cargotoml` parameter AND there is no Cargo.lock?
+            // ANSWER: Well, we just run `cargo_vendor`. *Vendoring generates Cargo.lock*.
+            // **We will just ask the user to do cargo_vendor first**.
+
+            // WARN: Another thing to look out for if there is no presence of Cargo.lock.
+            // This can be worked around by just running `cargo_vendor` since vendoring generates one.
+            // But at least the user still gets the same vendored tarball *if they disabled updates*
+
+            // NOTE: The full cargo audit command should use `-f` or `--file`. We do not need to copy
+            // whole tarball or directory to a temporary directory.
+            match this {
+                Src::Tar(srctar) => {
+                    println!("{:?}", srctar);
+                }
+                Src::Dir(srcdir) => {
+                    println!("{:?}", srcdir);
+                }
+            }
+            Ok(())
+        } else {
+            Err(AuditError {
+                msg: "Yes".to_string(),
+            })
+        }
+    } else {
+        Err(AuditError {
+            msg: "Yes".to_string(),
+        })
+    }
 }
