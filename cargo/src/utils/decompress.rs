1b5897348a3f43f3ddde065899fde9b72af26346
Soc Virnyl Estela <contact@uncomfyhalomacro.pl>
2024-03-30 23:49:01 +0800

    appreciation: all who contributed code to this project should be listed
    Signed-off-by: Soc Virnyl Estela <contact@uncomfyhalomacro.pl>

--- cargo/src/utils/decompress.rs
+++ cargo/src/utils/decompress.rs
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MPL-2.0
 
-// Copyright (C) 2023  Soc Virnyl Estela
+// Copyright (C) 2024 To all Contributors of this project listed in CONTRIBUTORS.md
 
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
