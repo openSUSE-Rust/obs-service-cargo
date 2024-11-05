755785cccc0d91bf9cb8a5ff5cf53e0f6db1049e
Soc Virnyl Estela <contact@uncomfyhalomacro.pl>
2024-11-05 14:09:44 +0800

    tests: update test suite in CI to conform with new code changes
    Signed-off-by: Soc Virnyl Estela <contact@uncomfyhalomacro.pl>

--- cargo/tests/behaviour.rs
+++ cargo/tests/behaviour.rs
@@ -1,7 +1,7 @@
 #![allow(clippy::unwrap_used)]
 
 use libroast::common::Compression;
-use obs_service_cargo::cli;
+use obs_service_cargo::cli::{self, Method, VendorArgs};
 use rand::prelude::*;
 use std::{io, path::PathBuf};
 use test_log::test;
@@ -29,23 +29,26 @@ async fn vendor_source(source: &str, filter: bool) -> io::Result<PathBuf> {
     let data = data.to_vec();
     fs::write(&outfile, data).await.unwrap();
     let outdir = PathBuf::from("/tmp");
-    let opt = cli::Opts {
-        src: cli::Src {
-            src: outfile.to_path_buf(),
-        },
-        compression: Compression::default(),
-        tag: Some(random_tag),
-        cargotoml: [].to_vec(),
-        update: true,
+    let vendor_specific_args = VendorArgs {
         filter,
-        outdir,
-        color: clap::ColorChoice::Auto,
-        i_accept_the_risk: [].to_vec(),
-        respect_lockfile: false,
         versioned_dirs: true,
     };
+    let opt = cli::Opts {
+        method: Method::Vendor,
+        custom_root: None,
+        no_root_manifest: false,
+        src: outfile.to_path_buf(),
+        compression: Compression::default(),
+        tag: Some(random_tag),
+        manifest_paths: vec![],
+        update: true,
+        vendor_specific_args,
+        outdir,
+        color: clap::ColorChoice::Auto,
+        i_accept_the_risk: vec![],
+    };
 
-    let res = opt.src.run_vendor(&opt).map_err(|err| {
+    let res = opt.run_vendor().map_err(|err| {
         error!(?err);
         io::Error::new(io::ErrorKind::Interrupted, err.to_string())
     });
@@ -71,9 +74,9 @@ async fn no_filter_vendor_sources() -> io::Result<()> {
 
 #[test(tokio::test)]
 async fn filter_vendor_sources() -> io::Result<()> {
-    let sources: &[&str] = &[
-        "https://github.com/zellij-org/zellij/archive/refs/tags/v0.40.1.tar.gz",
-        "https://github.com/alacritty/alacritty/archive/refs/tags/v0.14.0.tar.gz",
+    let sources = [
+        "https://github.com/elliot40404/bonk/archive/refs/tags/v0.3.2.tar.gz",
+        "https://github.com/openSUSE-Rust/roast/archive/refs/tags/v5.1.2.tar.gz",
     ];
     for src in sources {
         let _ = spawn(async move {
@@ -86,7 +89,7 @@ async fn filter_vendor_sources() -> io::Result<()> {
 }
 
 #[test(tokio::test)]
-async fn cargotoml_test_1() -> io::Result<()> {
+async fn vendor_registry_test_with_no_root_manifest() -> io::Result<()> {
     let source = "https://github.com/ibm-s390-linux/s390-tools/archive/refs/tags/v2.29.0.tar.gz";
     let mut rng = rand::thread_rng();
     let random_tag: u8 = rng.gen();
@@ -107,23 +110,26 @@ async fn cargotoml_test_1() -> io::Result<()> {
     let data = data.to_vec();
     fs::write(&outfile, data).await.unwrap();
     let outdir = PathBuf::from("/tmp");
-    let opt = cli::Opts {
-        src: cli::Src {
-            src: outfile.to_path_buf(),
-        },
-        compression: Compression::default(),
-        tag: Some(random_tag),
-        cargotoml: [PathBuf::from("rust/pvsecret/Cargo.toml")].to_vec(),
-        update: true,
+    let vendor_specific_args = VendorArgs {
         filter: true,
-        outdir,
-        color: clap::ColorChoice::Auto,
-        i_accept_the_risk: [].to_vec(),
-        respect_lockfile: false,
         versioned_dirs: true,
     };
+    let opt = cli::Opts {
+        custom_root: None,
+        no_root_manifest: true,
+        method: Method::Registry,
+        src: outfile.to_path_buf(),
+        compression: Compression::default(),
+        tag: Some(random_tag),
+        manifest_paths: [PathBuf::from("rust/pvsecret/Cargo.toml")].to_vec(),
+        update: true,
+        vendor_specific_args,
+        outdir,
+        color: clap::ColorChoice::Auto,
+        i_accept_the_risk: vec![],
+    };
 
-    let res = opt.src.run_vendor(&opt).map_err(|err| {
+    let res = opt.run_vendor().map_err(|err| {
         error!(?err);
         io::Error::new(io::ErrorKind::Interrupted, err.to_string())
     });
@@ -132,7 +138,7 @@ async fn cargotoml_test_1() -> io::Result<()> {
 }
 
 #[test(tokio::test)]
-async fn cargotoml_test_2() -> io::Result<()> {
+async fn manifest_paths_test_2() -> io::Result<()> {
     let source = "https://github.com/influxdata/flux/archive/refs/tags/v0.194.4.tar.gz";
     let mut rng = rand::thread_rng();
     let random_tag: u8 = rng.gen();
@@ -153,23 +159,75 @@ async fn cargotoml_test_2() -> io::Result<()> {
     let data = data.to_vec();
     fs::write(&outfile, data).await.unwrap();
     let outdir = PathBuf::from("/tmp");
-    let opt = cli::Opts {
-        src: cli::Src {
-            src: outfile.to_path_buf(),
-        },
-        compression: Compression::default(),
-        tag: Some(random_tag),
-        cargotoml: [PathBuf::from("libflux/Cargo.toml")].to_vec(),
-        update: true,
-        filter: true,
-        outdir,
-        color: clap::ColorChoice::Auto,
-        i_accept_the_risk: [].to_vec(),
-        respect_lockfile: false,
+    let vendor_specific_args = VendorArgs {
+        filter: false,
         versioned_dirs: true,
     };
+    let opt = cli::Opts {
+        no_root_manifest: false,
+        custom_root: None,
+        method: Method::Vendor,
+        src: outfile.to_path_buf(),
+        compression: Compression::default(),
+        tag: Some(random_tag),
+        manifest_paths: [PathBuf::from("libflux/Cargo.toml")].to_vec(),
+        update: true,
+        outdir,
+        color: clap::ColorChoice::Auto,
+        i_accept_the_risk: vec![],
+        vendor_specific_args,
+    };
 
-    let res = opt.src.run_vendor(&opt).map_err(|err| {
+    let res = opt.run_vendor().map_err(|err| {
+        error!(?err);
+        io::Error::new(io::ErrorKind::Interrupted, err.to_string())
+    });
+    assert!(res.is_ok());
+    Ok(())
+}
+
+#[test(tokio::test)]
+async fn custom_root_test() -> io::Result<()> {
+    let source = "https://github.com/influxdata/flux/archive/refs/tags/v0.194.4.tar.gz";
+    let mut rng = rand::thread_rng();
+    let random_tag: u8 = rng.gen();
+    let random_tag = random_tag.to_string();
+    let response = reqwest::get(source).await.unwrap();
+    let fname = response
+        .url()
+        .path_segments()
+        .and_then(|segments| segments.last())
+        .and_then(|name| if name.is_empty() { None } else { Some(name) })
+        .unwrap_or("balls");
+    info!("Source file: {}", &fname);
+    let outfile = format!("/{}/{}", "tmp", &fname);
+    info!("Downloaded to: '{:?}'", &outfile);
+    fs::File::create(&outfile).await.unwrap();
+    let outfile = PathBuf::from(&outfile);
+    let data = response.bytes().await.unwrap();
+    let data = data.to_vec();
+    fs::write(&outfile, data).await.unwrap();
+    let outdir = PathBuf::from("/tmp");
+    let vendor_specific_args = VendorArgs {
+        filter: false,
+        versioned_dirs: true,
+    };
+    let opt = cli::Opts {
+        no_root_manifest: false,
+        custom_root: Some("libflux".to_string()),
+        method: Method::Vendor,
+        src: outfile.to_path_buf(),
+        compression: Compression::default(),
+        tag: Some(random_tag),
+        manifest_paths: vec![],
+        update: true,
+        outdir,
+        color: clap::ColorChoice::Auto,
+        i_accept_the_risk: vec![],
+        vendor_specific_args,
+    };
+
+    let res = opt.run_vendor().map_err(|err| {
         error!(?err);
         io::Error::new(io::ErrorKind::Interrupted, err.to_string())
     });
