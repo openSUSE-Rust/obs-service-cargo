18a17da069998e8d5e4e7b7881edeaab83f2fd01
Soc Virnyl Estela <contact@uncomfyhalomacro.pl>
2024-11-16 10:15:07 +0800

    fix: add warning
    Signed-off-by: Soc Virnyl Estela <contact@uncomfyhalomacro.pl>

--- cargo/src/cli.rs
+++ cargo/src/cli.rs
@@ -212,6 +212,9 @@ impl Opts {
 
         // It won't make sense for update to be globally true while specifying to update a package
         if !&self.update_crate.is_empty() {
+            warn!(
+                "⚠️ Global update flag was set to false because specific crates to update are set!"
+            );
             self.update = false;
         }
 
